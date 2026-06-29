ffb2a97eb25a0e49e9502850dde02eeb7a3d6d0c
vivekranjan9 <sahoovivek09122002@hotmail.com>
2026-06-30 00:26:07 +0530

    Added Cesium 3D Globe

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 // ===============================
 
 // Paste YOUR token below
-Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwZjQ2NzhlMC1jNzJjLTQ1MzgtYmNkOS1iZmY2MjY0MDliN2UiLCJpZCI6NDUwNDk4LCJpc3MiOiJodHRwczovL2FwaS5jZXNpdW0uY29tIiwiYXVkIjoidW5kZWZpbmVkX2RlZmF1bHQiLCJpYXQiOjE3ODI3NTg1OTZ9.AXq0WsSeJ1Ise1sdavCwqX5tewErhntMHciAJUOkcGU";
+Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmYWM2YzdhMi1kYzFkLTRjYmUtOTkyZC04NDE0OTVjOTFkYzYiLCJpZCI6NDUwNDk4LCJzdWIiOiJ2aXZla3JhbmphbjkiLCJpc3MiOiJodHRwczovL2FwaS5jZXNpdW0uY29tIiwiYXVkIjoidml2ZWtyYW5qYW45X2RlZmF1bHQiLCJpYXQiOjE3ODI3NTkxNjZ9.ES-p0StL7k--QZVUJOq2sbWZHZX8734Et63SmBels0M";
 
 // Create Viewer
 const viewer = new Cesium.Viewer("cesiumContainer", {
