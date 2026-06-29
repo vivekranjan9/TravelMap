18bc40d7a0e78f95af119d38d359cdefb990f0a7
vivekranjan9 <sahoovivek09122002@hotmail.com>
2026-06-30 00:33:46 +0530

    Added Cesium 3D Globe

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // TravelMap v2.0
 // ===============================
 
-Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmYWM2YzdhMi1kYzFkLTRjYmUtOTkyZC04NDE0OTVjOTFkYzYiLCJpZCI6NDUwNDk4LCJzdWIiOiJ2aXZla3JhbmphbjkiLCJpc3MiOiJodHRwczovL2FwaS5jZXNpdW0uY29tIiwiYXVkIjoidml2ZWtyYW5qYW45X2RlZmF1bHQiLCJpYXQiOjE3ODI3NTkxNjZ9.ES-p0StL7k--QZVUJOq2sbWZHZX8734Et63SmBels0M";
+Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmM2ViM2QxNC1lYjY2LTRiM2YtOWQ1Ni1jMzRkNDkzMGU1OGYiLCJpZCI6NDUwNDk4LCJzdWIiOiJ2aXZla3JhbmphbjkiLCJpc3MiOiJodHRwczovL2FwaS5jZXNpdW0uY29tIiwiYXVkIjoidml2ZWtyYW5qYW45X2RlZmF1bHQiLCJpYXQiOjE3ODI3NTk4MDR9.VTGTfFHVnjPfpDmI2yLCQyiRyHU_Et5-s1Nf51Mg06U";
 
 // Create viewer with default globe
 const viewer = new Cesium.Viewer("cesiumContainer", {
